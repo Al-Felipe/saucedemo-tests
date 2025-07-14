28ca278a1e327f09e2f3933ecacce8d9b0b7947e
Felipe Alves <felipealves3580@gmail.com>
2025-07-14 16:27:09 -0300

    create BasePage

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+    public static WebDriver driver;
+
+    public void setDriver(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void set(By locator, String text){
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public void click(By locator){
+        driver.findElement(locator).click();
+    }
+}
